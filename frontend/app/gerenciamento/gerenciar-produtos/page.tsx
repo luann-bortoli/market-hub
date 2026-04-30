e2547049c3d388f3b57483e748e1de98271c5a77
Luann <luanndebortoli@gmail.com>
2026-04-29 21:28:32 -0300

    add settings card

---
+++ frontend/app/gerenciamento/gerenciar-produtos/page.tsx
@@ -0,0 +1,9 @@
+import s from './page.module.css'
+
+export default function GerenciarProdutos(){
+    return(
+        <>
+            aaa
+        </>
+    )
+}
---
+++ frontend/app/gerenciamento/layout.tsx
@@ -0,0 +1,29 @@
+import type { Metadata } from "next";
+import { Poppins } from "next/font/google";
+import "../globals.css";
+import Header from "@/components/header/Header";
+
+const poppins = Poppins({
+  variable: "--font-poppins",
+  subsets: ["latin"],
+  weight: ["400", "500", "600", "800"]
+});
+
+export const metadata: Metadata = {
+  title: "Market Hub",
+  description: "",
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en" suppressHydrationWarning={true}>
+      <body>
+        {children}
+      </body>
+    </html>
+  );
+}
